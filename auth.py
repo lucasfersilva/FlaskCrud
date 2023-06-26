fb3d3712785d207a45429fa5808bd1ff69a32693
Lucas Fernandes <lucasfernandes@MacBook-Air-de-Lucas.local>
2023-06-26 15:54:41 -0300

    implementei sessao de usuario, login e logout

--- auth.py
+++ auth.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect,url_for
 import requests as req
 import json
+from models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from init import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -27,14 +31,31 @@ def get_top_news():
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully',category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Senha incorreta, tente novamente', category='error')
+        else:
+            flash('Email não existe', category='error')
+
     data = request.form
     print(data)
     return render_template('login.html', boolean=True)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('home.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods =['GET','POST'])
@@ -45,8 +66,11 @@ def signup():
         print(firstname)
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
+        user = User.query.filter_by(email = email).first()
+        if user:
+            flash("This email already exists",category='error')
 
-        if len(email) < 4 or None:
+        elif len(email) < 4 or None:
             flash("Email tem que ser maior que 4 caracteres", category="error")
 
         elif len(firstname) <2:
@@ -56,7 +80,13 @@ def signup():
             flash("Suas senhas não coincide", category="error")
 
         else:
+            new_user = User(email=email, first_name=firstname, password= generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash("Conta Criada!", category="success")
+
+            return redirect(url_for('views.home'))
 
     return render_template('sign-up.html')
 
--- init.py
+++ init.py
@@ -1,15 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'LUCASFERNANDES'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from views import views
     import auth
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth.auth, url_prefix='/')
+    from models import User, Note
 
+    with app.app_context():
+        db.create_all()
+        print("database created")
     return app
+
+
