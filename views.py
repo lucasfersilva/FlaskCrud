ec6f69a666b2fc8fd48d6bf3bb525c7a47dd6bd4
Lucas Fernandes <lucasfernandes@MacBook-Air-de-Lucas.local>
2023-06-27 16:44:24 -0300

    implementado a exclusão de notas e uma funcao js para ajudar nas rotas

--- views.py
+++ views.py
@@ -19,6 +19,22 @@ def home():
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Nota adicionada',category='sucess')
+            flash('Nota adicionada',category='success')
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods =['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteid = note['noteId']
+    note = Note.query.get(noteid)
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+        flash('Nota excluída', category='success')
+    else:
+        flash('Nota invalida', category='error')
+
+    return render_template("home.html", user=current_user)
+
 
